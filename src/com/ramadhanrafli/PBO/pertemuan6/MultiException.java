5f967cd0a423755cd3cb8f09d7deafd65fb9921c
PBO_Ramadhan Rafli Pratama_21102271 <21102271@ittelkom-pwt.c.id>
2023-05-24 13:44:22 +0700

    pertemuan 6

---
+++ src/com/ramadhanrafli/PBO/pertemuan6/MultiException.java
@@ -0,0 +1,33 @@
+package com.ramadhanrafli.PBO.pertemuan6;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MultiException {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        try{
+            System.out.println("Masukkan bilangan : ");
+            int bilangan1 = input.nextInt();
+
+            System.out.println("Masukkan pembaginya : ");
+            int bilangan2 = input.nextInt();
+
+            int hasil = bilangan1/bilangan2;
+            System.out.println(bilangan1 + " / " + bilangan2+ " = " + hasil);
+        }
+//        catch (ArithmeticException | InputMismatchException e){
+//            System.out.println("Error gess. tidak dapat diproses :(");
+//        }
+
+        //Multiple catch
+        catch (ArithmeticException e){
+            System.out.println("error tidak dapat diproses");
+        }
+        catch (InputMismatchException e){
+            System.out.println("masukan angka saja");
+        }
+            System.out.println("proses sudah selesai :)");
+    }
+}

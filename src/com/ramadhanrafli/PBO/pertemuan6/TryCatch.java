f177c2682e36ba8024d748061028fd14a569895d
PBO_Ramadhan Rafli Pratama_21102271 <21102271@ittelkom-pwt.c.id>
2023-05-24 13:46:21 +0700

    pertemuan 6

---
+++ src/com/ramadhanrafli/PBO/pertemuan6/TryCatch.java
@@ -0,0 +1,15 @@
+package com.ramadhanrafli.PBO.pertemuan6;
+
+public class TryCatch {
+    public static void main(String[] args) {
+        //membuat array
+        int[] nomor = {1,2,3,4,5};
+        try {
+            System.out.println(nomor[5]);
+        }
+        catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("Arraynya melebihi batas");
+        }
+            System.out.println("Proses selesai");
+    }
+}

071fe0d4cf1d1fa2132a63791adeedc9d370d8d7
RamadhanRafliPratama <132367632+RamadhanRafliPratama@users.noreply.github.com>
2023-05-31 14:13:36 +0700

    Add files via upload

---
+++ src/com/ramadhanrafli/PBO/pertemuan7/Registrastion.java
@@ -0,0 +1,15 @@
+package com.ramadhanrafli.PBO.pertemuan7;
+
+import javax.swing.*;
+
+public class Registrastion {
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JRadioButton lakiLakiRadioButton;
+    private JRadioButton perempuanRadioButton;
+    private JTextField textField3;
+    private JTextArea textArea1;
+    private JButton saveButton;
+    private JButton clearButton;
+}
